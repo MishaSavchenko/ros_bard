e660d6f600b420471f816ddb5500eddc1b683f31
Misha Savchenko <savchenkmisha@gmail.com>
2024-01-03 08:00:39 -0600

    updated rclpy test, with some cleanup

--- scripts/rclpy_test.py
+++ scripts/rclpy_test.py
@@ -1,61 +1,64 @@
-import rclpy
-from rclpy.node import Node
-from std_msgs.msg import String
-from pprint import pprint
+#!/usr/bin/env python3
+import os
 import time
 import graphviz
-
-import numpy as np 
-from rclpy.impl.implementation_singleton import rclpy_implementation as _rclpy
-
-from ament_index_python.packages import get_package_share_directory
-
+from datetime import datetime
 from itertools import product
+
+import rclpy
+from rclpy.node import Node
+from ament_index_python.packages import get_package_share_directory
+from rclpy.impl.implementation_singleton import rclpy_implementation as _rclpy
 
 class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
 
-
 def main(args=None):
 
-
     # may raise PackageNotFoundError
-    package_share_directory = get_package_share_directory('my_package_name')
-    storage_directory = "/data"
+    package_share_directory = get_package_share_directory('ros_bard')
+    package_share_directory = "/home/misha/code/ros_bard_ws/src/ros_bard"
+    storage_directory = "data"
+    data_path = os.path.join(package_share_directory, storage_directory)
+    
+    if not os.path.exists(data_path):
+        raise OSError
+        quit()
 
-    # g.view()
+    print("Creating a directory in data...")
+
+    # Create a new directory to store log files 
+    mode = 0o755
+    new_dir_name = str(datetime.now()).replace(" ", "_")
+    path = os.path.join(data_path, new_dir_name) 
+    os.mkdir(path, mode) 
+    logging_path = path
+
     rclpy.init(args=args)
 
     minimal_publisher = MinimalPublisher()
 
     node_info = {}
+    
+    while True:  
+        new_file_name = str(datetime.now()).replace(" ", "_") + ".bv"
+        new_file = os.path.join(logging_path, new_file_name)
 
-    adjacency_matrix = []
-    adjacency_matrix = np.zeros((1, 1, 3))
-
-    topic_info = {}
-
-
-    while True: 
-
-
-        g = graphviz.Digraph('G', filename='hello.gv')
+        g = graphviz.Digraph('G', filename=new_file)
         g.attr('graph', 
                layout="sfdp", 
                beautify="false",
                voro_margin="1.0",
                repulsiveforce="10.0")
-        # g.attr('graph')  # use dot
         
         g.attr('node', shape="box")
         total_edges = {}
 
-
         topics = minimal_publisher.get_topic_names_and_types()
         for topic in topics: 
-            if "action" in topic[0] or "parameter_event" in topic[0]:
-                continue
+            # if "action" in topic[0] or "parameter_event" in topic[0]:
+            #     continue
 
             pub_info = minimal_publisher._get_info_by_topic(topic[0], False, _rclpy.rclpy_get_publishers_info_by_topic)
             sub_info = minimal_publisher._get_info_by_topic(topic[0], False, _rclpy.rclpy_get_subscriptions_info_by_topic)
@@ -76,78 +79,7 @@ def main(args=None):
                     g.edge(edge_definition[0], edge_definition[1], label=edge_definition[2])
 
         g.render()
-        # input("Waiting for you m'lord")
         time.sleep(1.0)
-            
-            # print("-----------------")
-            # for ti in pub_info + sub_info: 
-            #     print(ti)
-        # pprint(topics)
-        # quit()
-        # break
-
-
-    # 0: topic
-    # 1: service 
-    # 2: action
-
-    # while True: 
-    #     # namespaces = node_names_namespaces[:,1]
-    #     # topic_names_and_types = minimal_publisher.get_topic_names_and_types()
-    #     # pprint(topic_names_and_types)
-
-    #     node_names_namespaces = minimal_publisher.get_node_names_and_namespaces()
-    #     node_names = [nnn[0] for nnn in node_names_namespaces]
-    #     namespaces = [nnn[1] for nnn in node_names_namespaces]
-
-    #     adjacency_matrix.resize(len(node_names), len(node_names), 3)
-
-    #     for indx, node_name in enumerate(node_names):
-    #         # print("--------------------------------------")
-
-    #         publishers = minimal_publisher.get_publisher_names_and_types_by_node(node_names[indx], 
-    #                                                                              namespaces[indx])
-            
-    #         subscribers = minimal_publisher.get_subscriber_names_and_types_by_node(node_names[indx], 
-    #                                                                                namespaces[indx])
-
-
-
-
-    #         if node_name not in node_info:
-    #             node_info[node_name] = {"pub":{"name": None, "type":None}, "sub":{"name": None, "type":None}}
-
-    #         node_info[node_name]["pub"]["name"] = [p[0] for p in publishers]
-    #         node_info[node_name]["pub"]["type"] = [p[1][0] for p in publishers]
-
-    #         node_info[node_name]["sub"]["name"] = [s[0] for s in subscribers]
-    #         node_info[node_name]["sub"]["type"] = [s[1][0] for s in subscribers]
-
-    #     break
-
-    # adjacency_matrix = np.zeros((len(node_info), len(node_info), 3))
-    # for node_name, v in node_info.items():
-    #     pubs = v["pub"]["name"]
-    #     subs = v["sub"]["name"]
-    #     print(pubs)
-    #     print(subs)
-    #     # print(node_name)
-    #     # print(v)
-    # # print(adjacency_matrix)
-    # # pprint(node_info)
-
-    quit()
-    g = graphviz.Graph('G', filename='hello.gv')
-    g.attr('node', shape="box")
-
-    g.render()
-    time.sleep(0.25)
-    
-    rclpy.spin_once(minimal_publisher)
-    rclpy.spin(minimal_publisher)
-    minimal_publisher.destroy_node()
-    rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
